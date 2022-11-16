fa03b4e1563976d5fc227949134f911f240b476f
amiyuki7 <amiyuki788@gmail.com>
2022-11-17 10:03:38 +1100

    Abstract into a try/except/finally

--- src/redqct/app.py
+++ src/redqct/app.py
@@ -1,4 +1,5 @@
 from multiprocessing import Process, Pipe
+from colorama import Fore, Style
 
 from .bot import main as client_main
 from .server import main as server_main
@@ -15,8 +16,18 @@ server_proc = Process(
     args=(server_end,),
 )
 
+
+def main() -> None:
+    try:
+        client_proc.start()
+        server_proc.start()
+        client_proc.join()
+        server_proc.join()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print(Fore.GREEN + Style.BRIGHT + "~~~ Shutting down bot & server ~~~" + Style.RESET_ALL)
+
+
 if __name__ == "__main__":
-    client_proc.start()
-    server_proc.start()
-    client_proc.join()
-    server_proc.join()
+    main()
