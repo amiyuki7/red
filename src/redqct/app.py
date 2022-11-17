e55c303629d0c701ebfa188d8dc718c233ada6f9
amiyuki7 <amiyuki788@gmail.com>
2022-11-18 08:30:14 +1100

    Refactor: Isolate server and bot into two files

--- src/redqct/app.py
+++ src/redqct/app.py
@@ -1,23 +1,11 @@
 import asyncio
-import discord
-import os
-from dotenv import load_dotenv
-from discord.ext.commands import Bot, Context
+
 from fastapi import FastAPI
 from .lib import cube
-
-load_dotenv()
-# Set this in .env
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-# BOT_TOKEN = os.getenv("DEV_TOKEN")
-assert type(BOT_TOKEN) is str
+from .bot import bot, main
 
 
 server = FastAPI()
-
-intents = discord.Intents.default()
-intents.message_content = True
-bot = Bot(command_prefix="$", intents=intents)
 
 
 @server.get("/")
@@ -28,23 +16,6 @@ async def root_route():
 @server.get("/kanye")
 async def kanye_route():
     await bot.get_channel(1042389563009683496).send("Somebody just did a GET request @ 127.0.0.1:8000/kanye")
-
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
-
-
-@bot.command()
-async def kanye(ctx: Context):
-    await ctx.send(f"{ctx.author.display_name} thinks that kanye is king :crown:")
-
-
-async def main():
-    try:
-        await bot.start(BOT_TOKEN)
-    except KeyboardInterrupt:
-        await bot.close()
 
 
 asyncio.create_task(main())
---
+++ src/redqct/bot.py
@@ -0,0 +1,31 @@
+import os
+import discord
+from discord.ext.commands import Bot, Context
+from dotenv import load_dotenv
+
+load_dotenv()
+# Set this in .env
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+# BOT_TOKEN = os.getenv("DEV_TOKEN")
+assert type(BOT_TOKEN) is str
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = Bot(command_prefix="$", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+
+@bot.command()
+async def kanye(ctx: Context):
+    await ctx.send(f"{ctx.author.display_name} thinks that kanye is king :crown:")
+
+
+async def main():
+    try:
+        await bot.start(BOT_TOKEN)
+    except KeyboardInterrupt:
+        await bot.close()
