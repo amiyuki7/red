9db86f6ec2db98105d580848cf934663669cc41e
Vladimir Pak <vladimir.pak@student.scots.college>
2022-11-20 18:53:15 +1100

    Add bot debug attributes

--- src/redqct/bot.py
+++ src/redqct/bot.py
@@ -2,15 +2,16 @@ import os
 import discord
 from discord.ext.commands import Bot, Context
 from dotenv import load_dotenv
+from time import time
 
 load_dotenv()
 # Set this in .env
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("DEV_TOKEN")
 # BOT_TOKEN = os.getenv("DEV_TOKEN")
 assert type(BOT_TOKEN) is str
 
 intents = discord.Intents().all()
-bot = Bot(command_prefix="$", intents=intents)
+bot = Bot(command_prefix="%", intents=intents)
 
 
 @bot.event
@@ -21,6 +22,88 @@ async def on_ready():
 @bot.command()
 async def kanye(ctx: Context):
     await ctx.send(f"{ctx.author.display_name} thinks that kanye is king :crown:")
+
+
+@bot.command()
+async def specs_of(ctx: Context, member: discord.Member):
+    # await ctx.send("One Second")
+    member_name = member.name
+    await ctx.send(member_name)
+
+    member_tag = member.discriminator
+    await ctx.send(member_tag)
+
+    if member.nick:
+        member_nick = member.nick
+    else:
+        member_nick = "No nick"
+    await ctx.send(member_nick)
+    
+    if member.avatar:
+        member_avatar = member.display_avatar
+    else:
+        # see lib.py for explanation of default avatar colours
+        # print("https://cdn.discordapp.com/embed/avatars/" str(member.discriminator%5) + ".png")
+        print(str(int(member.discriminator)%5))
+        member_avatar = "https://cdn.discordapp.com/embed/avatars/" + member.discriminator%5 + ".png"
+        
+    await ctx.send(member_avatar)
+    
+    member_status = member.status
+    await ctx.send(member_status)
+    
+    if member.public_flags:
+        member_badges = member.public_flags
+    else:
+        member_badges = "No badges"
+    await ctx.send(member_badges)
+    
+    if member.activity:
+        member_activity_name = member.activity.name
+    else:
+        member_activity_name = "No activity"
+    await ctx.send(member_activity_name)
+
+    if member.activity.details:
+        member_activity_details = member.activity.details
+    await ctx.send(member_activity_details)
+
+    if member.activity:
+        member_activity_type = member.activity.type.name
+    else:
+        member_activity_type = ""
+    await ctx.send(member_activity_type)
+
+    if member.activity:
+        pass
+        # if isinstance(member.activity, discord.):
+        # member_activity_misc = [member.activity.state, member.activity.session_id]
+    
+
+    # start_time = member.activity.timestamps["start"]
+    # end_time = member.activity.timestamps["end"]
+
+    # # this is in milliseconds, but we can use a dating library to convert it nicely instead of writing by ourselves
+    # now = time.time()
+    # remaining = f"Time remaining (ms): {end_time - now}"
+    # elapsed = f"Time elapsed (ms): {now - start_time}"
+
+
+    # # Discord api has messaging rate limits so this should send everything in one chunk
+    # # i split it up so i could comment out individual things during bug fixing
+    # # for example aydan doesn't have activity details, so the entire thing breaks 
+    # # sine its in an f-string now, its just gonna show "None" instead of breaking... i think
+    # await ctx.send(f"{member_name}\n{member_tag}\n{member_nick}\n{member_avatar}\n{member_status}\n{member_badges}\n{member_activity_name}\n{member_activity_details}\n{member_activity_type}\n{remaining}\n{elapsed}")
+
+
+    # Dw about this chunk for now
+    # for i in member_activity_misc:
+    #     await ctx.send(i)
+
+    # member_specs = [member_name, member_tag, member_nick, member_avatar, member_status, member_badges, member_activity, member_activity_type]
+    # for spec in member_specs:
+    #     print(spec)
+    # await ctx.send(f"{member_name, member_tag, member_nick, member_avatar, member_status, member_badges, member_activity, member_activity_type}") 
 
 
 async def main():
--- src/redqct/lib.py
+++ src/redqct/lib.py
@@ -1,7 +1,51 @@
 from __future__ import annotations
+from typing import List
+import discord
 
 Number = int | float
 
+
+
+class Attrs:
+    # Workflow of this would be:
+    """
+    1. The webserver requests for something, e.g. 127.0.0.1:8080/id/703204753743806585
+    2. The route code in app.py  asks the bot to run a function that collects all the data for this specific user ID, and stores it into this Attrs class
+    3. The instance of Attrs is then passed to code in image.py for an image to be generated, and that image is given back to the webserver
+    4. The webserver sends that image back out to the user as well as a 200 OK code, and a 403 code if the ID cant be found
+    i have started writing attrs below alr
+    member_atributes
+    """
+    def __init__(self,name,tag,nick) -> None:
+        self.name = name
+        self.tag = tag
+        self.nick = nick
+        # etc
+    
+
+class member_atributes():
+    def __init__(self, name: str, tag: str, nick: str | None, avatar: discord.Member.avatar, status: str, badges: List[discord.PublicUserFlags] | None, activity: activity_atributes | None) -> None:
+        self.name = name
+        self.status = status
+        self.badges = badges
+        self.nick = nick
+        if avatar:
+            self.avatar = avatar
+        else:
+            # There are 5 base avatars, each with a different background colour. The colour is determined based on the discriminant
+            # For user name#1234 the discriminant would be 1234
+            # This is explained at https://www.reddit.com/r/discordapp/comments/au6v4e/how_to_change_your_defualt_discord_avatars_colour/
+            self.avatar = f"https://cdn.discordapp.com/embed/avatars/{tag % 5}.png";
+        self.activity = activity
+
+class activity_atributes():
+    def __init__(self, name: str, image_large: str, image_small: str, line1: str | None, line2: str | None, line3: str | None) -> None:
+        self.name = name
+        self.image_large = image_large
+        self.image_small = image_small
+        self.line1 = line1
+        self.line2 = line2
+        self.line3 = line3  
 
 def cube(x: Number) -> Number:
     """
