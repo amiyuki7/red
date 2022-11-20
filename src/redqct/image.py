84b616e47b49fe01e70cf3298951d68697763052
amiyuki7 <amiyuki788@gmail.com>
2022-11-20 15:55:39 +1100

    Add text generation

--- src/redqct/image.py
+++ src/redqct/image.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 from typing import Tuple
 
 
@@ -17,6 +17,12 @@ class Cache_:
         "light_idle",
         "light_dnd",
         "light_invis",
+        "bold_30",
+        "bold_25",
+        "bold_20",
+        "heavy_25",
+        "reg_25",
+        "noto_25",
     )
 
     def __init__(self) -> None:
@@ -30,6 +36,13 @@ class Cache_:
         self.light_idle = Image.open(f"{ROOT_DIR}/assets/status_idle.png").resize((48, 47))
         self.light_dnd = Image.open(f"{ROOT_DIR}/assets/status_dnd.png").resize((48, 47))
         self.light_invis = Image.open(f"{ROOT_DIR}/assets/status_invis.png").resize((48, 47))
+
+        self.bold_30 = ImageFont.truetype(f"{ROOT_DIR}/fonts/Uni Sans Bold.ttf", 30)
+        self.bold_25 = ImageFont.truetype(f"{ROOT_DIR}/fonts/Uni Sans Bold.ttf", 25)
+        self.bold_20 = ImageFont.truetype(f"{ROOT_DIR}/fonts/Uni Sans Bold.ttf", 20)
+        self.heavy_25 = ImageFont.truetype(f"{ROOT_DIR}/fonts/Uni Sans Heavy.ttf", 25)
+        self.reg_25 = ImageFont.truetype(f"{ROOT_DIR}/fonts/Uni Sans.ttf", 25)
+        self.noto_25 = ImageFont.truetype(f"{ROOT_DIR}/fonts/NotoSansMonoCJK-VF.ttf", 25)
 
 
 Cache = Cache_()
@@ -51,6 +64,9 @@ class Template:
 
     def peek(self) -> None:
         self._background.show()
+
+    def to_editable(self) -> ImageDraw.ImageDraw:
+        return ImageDraw.Draw(self._background)
 
 
 def masked(img: Image.Image, mask: Image.Image) -> Image.Image:
@@ -97,6 +113,69 @@ def main():
     # Light
     template.draw(Cache.status_underlay, (164, 184))
     template.draw(Cache.light_dnd, (176, 196))
+
+    # An interface which allows text to be directly edited into the template
+    edit = template.to_editable()
+
+    has_nick = True
+    username = "Gud"
+    tag = "3093"
+    nickname = "Vlad"
+    username_xy = (266, 113)
+    username_xy2 = (266, 73)
+    nickname_xy = (266, 109)
+
+    if has_nick:
+        # Draw the name#tag and the nickname underneath it
+        # name#tag
+        edit.text(username_xy2, username, fill=(255, 255, 255), font=Cache.bold_30)
+
+        left_margin = edit.textsize(username, font=Cache.bold_30)[0]
+
+        edit.text(
+            (username_xy2[0] + left_margin, username_xy2[1]),
+            f"#{tag}",
+            fill=(167, 169, 172),
+            font=Cache.bold_30,
+        )
+
+        # nickname
+        edit.text(nickname_xy, f"(aka {nickname})", fill=(255, 255, 255), font=Cache.bold_20)
+    else:
+        # Only draw the name#tag
+        # name#tag
+        edit.text(username_xy, username, fill=(255, 255, 255), font=Cache.bold_30)
+
+        left_margin = edit.textsize(username, font=Cache.bold_30)[0]
+
+        edit.text(
+            (username_xy[0] + left_margin, username_xy[1]),
+            f"#{tag}",
+            fill=(167, 169, 172),
+            font=Cache.bold_30,
+        )
+
+    # Activity text
+    activity_ = "PLAYING A GAME"
+    edit.text((51, 317), activity_, fill=(255, 255, 255), font=Cache.heavy_25)
+
+    line1 = "YouTube Music"
+    edit.text(
+        (253, 384),
+        line1,
+        fill=(255, 255, 255),
+        # If the string is ascii, use Uni Sans. Else, use Noto
+        font=[Cache.noto_25, Cache.reg_25][line1.isascii()],
+    )
+
+    line2 = "青春切符"
+    edit.text((253, 414), line2, fill=(255, 255, 255), font=[Cache.noto_25, Cache.reg_25][line2.isascii()])
+
+    line3 = "まふまふ-青春切符"
+    edit.text((253, 444), line3, fill=(255, 255, 255), font=[Cache.noto_25, Cache.reg_25][line3.isascii()])
+
+    line4 = "03:18 left"
+    edit.text((253, 474), line4, fill=(255, 255, 255), font=[Cache.noto_25, Cache.reg_25][line4.isascii()])
 
     template.peek()
 
