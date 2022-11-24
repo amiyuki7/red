b0d06679c6aff4ea7513c2ef4007d4bbee9f6436
amiyuki7 <amiyuki788@gmail.com>
2022-11-24 13:38:46 +1100

    Add data structure for user activity tracking

--- src/redqct/lib.py
+++ src/redqct/lib.py
@@ -7,6 +7,45 @@ import aiohttp
 Number = int | float
 
 
+class Users:
+    def __init__(self) -> None:
+        self.users: List[TrackedUser] = []
+
+    def track(self, user: str) -> None:
+        self.users.append(
+            TrackedUser(
+                id=user,
+            )
+        )
+
+    def update_graphs(self) -> None:
+        """
+        Should be called every minute by a discord bot loop
+        """
+        for user in self.users:
+            user.check_clear()
+            user.update_graph()
+
+
+class TrackedUser:
+    def __init__(self, id: str) -> None:
+        self.id = id
+        self.utc_offset: int = 0
+        self.img_path: str = ""
+
+    def check_clear(self) -> None:
+        """ """
+        # If it's exactly midnight for the user (relative to their utc offset), clear their graph
+        pass
+
+    def update_graph(self) -> None:
+        """ """
+        # Get the current time relative to the user's desired utc offset
+        # Get the user's current activities via the bot
+        # Draw a 1px line at that specific time in a specific colour depending on the activities present now
+        pass
+
+
 class MemberAttrs:
     def __init__(
         self,
@@ -51,9 +90,7 @@ class ActivityAttrs:
         self.line4 = len(line4) > 35 and line4[:50] + "..." or line4
 
 
-async def fetch_bytes(
-    session: aiohttp.ClientSession, url: str, id: str
-) -> Tuple[bytes, str]:
+async def fetch_bytes(session: aiohttp.ClientSession, url: str, id: str) -> Tuple[bytes, str]:
     async with session.get(url) as response:
         # Debug
         print(response.status, response.content_type)
